b6cc6b2fef42e2104e433e8c01accf352ef5dcb1
heitorsoares12 <heitor.soaress@outlook.com>
2024-06-27 23:16:25 -0300

    ajustando menu e inserindo logo

--- src/app/events/event-list/event-list.component.ts
+++ src/app/events/event-list/event-list.component.ts
@@ -15,7 +15,37 @@ interface Event {
 export class EventListComponent implements OnInit {
   events: Event[] = [
     {
-      image: 'assets/images/event1.jpg',
+      image: 'assets/images/event1.jfif',
+      title: 'Festa Sunset',
+      date: new Date('2024-07-15'),
+      location: 'Praia de Copacabana, RJ'
+    },
+    {
+      image: 'assets/images/event2.jpg',
+      title: 'Balada Eletrônica',
+      date: new Date('2024-08-10'),
+      location: 'Clube XYZ, SP'
+    },
+    {
+      image: 'assets/images/event3.jpg',
+      title: 'Festa Sunset',
+      date: new Date('2024-07-15'),
+      location: 'Praia de Copacabana, RJ'
+    },
+    {
+      image: 'assets/images/event4.jpg',
+      title: 'Balada Eletrônica',
+      date: new Date('2024-08-10'),
+      location: 'Clube XYZ, SP'
+    },
+    {
+      image: 'assets/images/event5.jpg',
+      title: 'Balada Eletrônica',
+      date: new Date('2024-08-10'),
+      location: 'Clube XYZ, SP'
+    },
+    {
+      image: 'assets/images/event2.jfif',
       title: 'Festa Sunset',
       date: new Date('2024-07-15'),
       location: 'Praia de Copacabana, RJ'
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { faHome, faCalendarAlt, faInfoCircle, faEnvelope, faMoon, faSun, faUser 
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  @HostBinding('attr.data-theme') theme = 'light';
+  @HostBinding('attr.data-theme') theme = 'dark';
   faHome = faHome;
   faCalendarAlt = faCalendarAlt;
   faInfoCircle = faInfoCircle;
@@ -18,68 +18,8 @@ export class NavbarComponent {
   faUser = faUser;
   showEventsMenu = false;
   showMobileMenu = false;
-  isDarkTheme = false;
 
-
-
-  // Método para alternar a exibição do menu móvel
-  toggleMobileMenu() {
-    this.showMobileMenu = !this.showMobileMenu;
-  }
-
-  toggleTheme() {
-    this.isDarkTheme = !this.isDarkTheme;
-    this.theme = this.theme === 'light' ? 'dark' : 'light'; // Alterna entre 'light' e 'dark'
-    localStorage.setItem('theme', this.theme); // Salva o tema escolhido no localStorage
-    if(this.theme == 'light'){
-      document.documentElement.setAttribute('data-theme', 'dark');
-    }
-    else{
-      document.documentElement.setAttribute('data-theme', 'light');
-    }
-  }
-
-  // Método para carregar o tema salvo anteriormente
-  loadSavedTheme() {
-    const savedTheme = localStorage.getItem('theme');
-    this.isDarkTheme = savedTheme === 'dark';
-    this.theme = savedTheme || 'light'; // Carrega o tema salvo ou define como 'light' se não houver tema salvo
-
-  }
-
-
-
-  // toggleTheme() {
-  //   this.isDarkTheme = !this.isDarkTheme;
-
-  //   // Adicionar ou remover a classe de tema no elemento raiz (por exemplo, body)
-  //   if (this.isDarkTheme) {
-  //     document.body.classList.add('dark-theme');
-  //     document.body.classList.remove('light-theme');
-  //   } else {
-  //     document.body.classList.add('light-theme');
-  //     document.body.classList.remove('dark-theme');
-  //   }
-
-  //   // Lógica adicional para persistir o tema (usando local storage, por exemplo)
-  //   localStorage.setItem('theme', this.isDarkTheme ? 'dark' : 'light');
-  // }
-
-  // // Método para carregar o tema salvo anteriormente
-  // loadSavedTheme() {
-  //   const savedTheme = localStorage.getItem('theme');
-  //   this.isDarkTheme = savedTheme === 'dark';
-  //   this.theme = savedTheme || 'light';
-  //   if (this.isDarkTheme) {
-  //     document.body.classList.add('dark-theme');
-  //   } else {
-  //     document.body.classList.add('light-theme');
-  //   }
-  // }
-
-  // Chamada ao carregar o componente para carregar o tema salvo
   ngOnInit() {
-    this.loadSavedTheme();
-    document.documentElement.setAttribute('data-theme', 'light');
+    document.documentElement.setAttribute('data-theme', 'dark');
   }
 }
